7e2d5b1e860eb463edbda983bfa91e4064b8a14d
John Parton <john.parton.iv@gmail.com>
2024-01-24 14:35:38 -0600

    Update test case for Django 5.0

--- test_app/models.py
+++ test_app/models.py
@@ -6,6 +6,8 @@ class TestModel(models.Model):
     image = models.ImageField(
         upload_to='images/', 
         storage=NoReadFileSystemStorage(),
+        width_field="width",
+        height_field="height",
     )
     width = models.IntegerField()
     height = models.IntegerField()
